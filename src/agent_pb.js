979f5eefad54fb79e9fd3f87fa1cf9e1c3d964c0
VDaaS org <ci@vdaas.org>
2020-05-11 06:41:04 +0000

    [skip ci] Update [vald sha: 6a8c616f1be31a444e448f6e301c5aedde8a63cd]

--- src/agent_pb.js
+++ src/agent_pb.js
@@ -1,9 +1,6 @@
-// source: agent.proto
 /**
  * @fileoverview
  * @enhanceable
- * @suppress {messageConventions} JS Compiler reports an error if a variable or
- *     field starts with 'MSG_' and isn't a translatable message.
  * @public
  */
 // GENERATED CODE -- DO NOT EDIT!
@@ -13,6 +10,5 @@ var goog = jspb;
 var global = Function('return this')();
 
 var payload_pb = require('./payload_pb.js');
-goog.object.extend(proto, payload_pb);
 var google_api_annotations_pb = require('./google/api/annotations_pb.js');
-goog.object.extend(proto, google_api_annotations_pb);
+goog.object.extend(exports, proto.agent);
