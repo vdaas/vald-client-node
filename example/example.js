7952f349afc21fbc617c42bce5875620f726e8c5
Rintaro Okamura <rokamura@yahoo-corp.jp>
2020-10-22 15:12:16 +0900

    :memo: update example
    Signed-off-by: Rintaro Okamura <rokamura@yahoo-corp.jp>

--- example/example.js
+++ example/example.js
@@ -2,27 +2,59 @@ var grpc = require('grpc');
 var vald = require('vald-client-node');
 
 var insert = vald.v1_vald.insert;
+var search = vald.v1_vald.search;
+var update = vald.v1_vald.update;
+var remove = vald.v1_vald.remove;
+
 var payload = vald.v1_payload.payload;
 
-var client = new insert.grpc.InsertClient('localhost:8081', grpc.credentials.createInsecure());
+// create clients
+var iclient = new insert.grpc.InsertClient('localhost:8081', grpc.credentials.createInsecure());
+var sclient = new search.grpc.SearchClient('localhost:8081', grpc.credentials.createInsecure());
+var uclient = new update.grpc.UpdateClient('localhost:8081', grpc.credentials.createInsecure());
+var rclient = new remove.grpc.RemoveClient('localhost:8081', grpc.credentials.createInsecure());
 
-var vec = new payload.Object.Vector();
-vec.setId("id_1");
-vec.setVectorList([0.1, 0.2, 0.3, 0.4]);
+
+// insert
+
+var ivec = new payload.Object.Vector();
+ivec.setId("id_1");
+ivec.setVectorList([0.1, 0.2, 0.3, 0.4]);
 
 var icfg = new payload.Insert.Config();
 icfg.setSkipStrictExistCheck(false);
 
 var ireq = new payload.Insert.Request();
-ireq.setVector(vec);
+ireq.setVector(ivec);
 ireq.setConfig(icfg);
 
-client.insert(ireq, function (err, resp) {
+iclient.insert(ireq, function (err, resp) {
     if (err != null) {
         console.log('error: ', err);
-        return
+    } else {
+        // resp is Object.Location
+        console.log('resp: ', resp);
     }
+});
 
-    // resp is Object.Location
-    console.log('resp: ', resp);
+
+// search
+
+var scfg = new payload.Search.Config();
+scfg.setNum(10);
+scfg.setRadius(-1.0);
+scfg.setEpsilon(0.01);
+scfg.setTimeout(3000000000);
+
+var sreq = new payload.Search.Request();
+sreq.setVectorList([0.1, 0.2, 0.3, 0.4]);
+sreq.setConfig(scfg);
+
+sclient.search(sreq, function (err, resp) {
+    if (err != null) {
+        console.log('error: ', err);
+    } else {
+        // resp is Search.Response
+        console.log('resp: ', resp);
+    }
 });
