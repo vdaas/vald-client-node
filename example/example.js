e9242755669c13bb8260395f4000bcd3d1df2841
Rintaro Okamura <rokamura@yahoo-corp.jp>
2020-10-26 16:49:20 +0900

    :recycle: revise example codes
    Signed-off-by: Rintaro Okamura <rokamura@yahoo-corp.jp>

--- example/example.js
+++ example/example.js
@@ -1,32 +1,30 @@
-var grpc = require('grpc');
-var vald = require('vald-client-node');
+const grpc = require('grpc');
+const vald = require('vald-client-node');
 
-var insert = vald.v1_vald.insert_grpc;
-var search = vald.v1_vald.search_grpc;
-var update = vald.v1_vald.update_grpc;
-var remove = vald.v1_vald.remove_grpc;
+const insert = vald.v1_vald.insert_grpc;
+const search = vald.v1_vald.search_grpc;
 
-var payload = vald.v1_payload.payload;
+const payload = vald.v1_payload.payload;
 
 // create clients
-var iclient = new insert.InsertClient('localhost:8081', grpc.credentials.createInsecure());
-var sclient = new search.SearchClient('localhost:8081', grpc.credentials.createInsecure());
+let iclient = new insert.InsertClient('localhost:8081', grpc.credentials.createInsecure());
+let sclient = new search.SearchClient('localhost:8081', grpc.credentials.createInsecure());
 
 
 // insert
 
-var ivec = new payload.Object.Vector();
+let ivec = new payload.Object.Vector();
 ivec.setId("id_1");
 ivec.setVectorList([0.1, 0.2, 0.3, 0.4]);
 
-var icfg = new payload.Insert.Config();
+let icfg = new payload.Insert.Config();
 icfg.setSkipStrictExistCheck(false);
 
-var ireq = new payload.Insert.Request();
+let ireq = new payload.Insert.Request();
 ireq.setVector(ivec);
 ireq.setConfig(icfg);
 
-iclient.insert(ireq, function (err, resp) {
+iclient.insert(ireq, (err, resp) => {
     if (err != null) {
         console.log('error: ', err);
     } else {
@@ -38,17 +36,17 @@ iclient.insert(ireq, function (err, resp) {
 
 // search
 
-var scfg = new payload.Search.Config();
+let scfg = new payload.Search.Config();
 scfg.setNum(10);
 scfg.setRadius(-1.0);
 scfg.setEpsilon(0.01);
 scfg.setTimeout(3000000000);
 
-var sreq = new payload.Search.Request();
+let sreq = new payload.Search.Request();
 sreq.setVectorList([0.1, 0.2, 0.3, 0.4]);
 sreq.setConfig(scfg);
 
-sclient.search(sreq, function (err, resp) {
+sclient.search(sreq, (err, resp) => {
     if (err != null) {
         console.log('error: ', err);
     } else {
