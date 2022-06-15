39c4b933dc9fdebeafb91ec9cfd609c3108ae023
Kiichiro YUKAWA <kyukawa315@gmail.com>
2022-06-15 10:12:37 +0900

    Use official package for example code (#18)
    * :recycle: use offical package for example code
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :recycle: add the wait for createIndex func
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :recycle: fix example codes
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- example-ts/example.ts
+++ example-ts/example.ts
@@ -1,48 +1,86 @@
-import grpc = require('grpc');
-import { v1_vald, v1_payload } from 'vald-client-node';
+import * as grpc from "@grpc/grpc-js";
+import { v1_vald, v1_payload } from "vald-client-node";
 
 // create clients
 
-const iclient = new v1_vald.insert_grpc.InsertClient('localhost:8081', grpc.credentials.createInsecure());
-const sclient = new v1_vald.search_grpc.SearchClient('localhost:8081', grpc.credentials.createInsecure());
+const iclient = new v1_vald.insert_grpc.InsertClient(
+  "localhost:8081",
+  grpc.credentials.createInsecure()
+);
+const sclient = new v1_vald.search_grpc.SearchClient(
+  "localhost:8081",
+  grpc.credentials.createInsecure()
+);
 
-// insert
-
-const ivec = new v1_payload.payload.Object.Vector();
-ivec.setId("id_1");
-ivec.setVectorList([0.1, 0.2, 0.3, 0.4]);
-
-const icfg = new v1_payload.payload.Insert.Config();
-icfg.setSkipStrictExistCheck(false);
-
-const ireq = new v1_payload.payload.Insert.Request();
-ireq.setVector(ivec);
-ireq.setConfig(icfg);
-
-iclient.insert(ireq, (err, resp) => {
-    if (err) {
-        console.log('error: ', err);
-    } else {
-        console.log('resp: ', resp);
-    }
+const sleep = async (second: number) => new Promise((resolve) => {
+  console.log('Wait for ', second, 's');
+  setTimeout(resolve, second * 1000);
 });
 
-// search
+const main = async () => {
+  // insert
 
-const scfg = new v1_payload.payload.Search.Config();
-scfg.setNum(10);
-scfg.setRadius(-1.0);
-scfg.setEpsilon(0.01);
-scfg.setTimeout(3000000000);
+  const ivec = new v1_payload.payload.Object.Vector();
+  ivec.setId("id_1");
+  ivec.setVectorList([0.1, 0.2, 0.3, 0.4]);
 
-const sreq = new v1_payload.payload.Search.Request();
-sreq.setVectorList([0.1, 0.2, 0.3, 0.4]);
-sreq.setConfig(scfg);
+  const icfg = new v1_payload.payload.Insert.Config();
+  icfg.setSkipStrictExistCheck(false);
 
-sclient.search(sreq, (err, resp) => {
-    if (err) {
-        console.log('error: ', err);
-    } else {
-        console.log('resp: ', resp);
-    }
-});
+  const ireq = new v1_payload.payload.Insert.Request();
+  ireq.setVector(ivec);
+  ireq.setConfig(icfg);
+
+
+  const insertFunc = (req: any) => {
+    return new Promise((resolve, reject) => {
+      iclient.insert(req, (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(resp);
+        }
+      });
+    });
+  };
+  insertFunc(ireq).then(async (res) => {
+    console.log("resp: ", res);
+  }).catch((e) => {
+    console.log("err: ", e);
+    return
+  });
+
+  const second = 100
+  await sleep(second);
+
+  // search
+  const scfg = new v1_payload.payload.Search.Config();
+  scfg.setNum(10);
+  scfg.setRadius(-1.0);
+  scfg.setEpsilon(0.01);
+  scfg.setTimeout(3000000000);
+
+  const sreq = new v1_payload.payload.Search.Request();
+  sreq.setVectorList([0.1, 0.2, 0.3, 0.4]);
+  sreq.setConfig(scfg);
+
+  const searchFunc = (req: any) => {
+    return new Promise((resolve, reject) => {
+      sclient.search(req, (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(resp);
+        }
+      });
+    });
+  };
+  searchFunc(sreq).then((res: any) => {
+    console.log('res: ', res, '\n');
+  }).catch((e) => {
+    console.log('err: ', e);
+    return
+  });
+};
+
+main();
