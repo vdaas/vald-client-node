e67b2365c5b8419bebc1459496da456b67f607dd
Kiichiro YUKAWA <kyukawa315@gmail.com>
2022-06-23 13:15:37 +0900

    :green_heart: Update CI (#19)
    * :green_heart: check
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :green_heart: fix
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :green_heart: fix
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :green_heart: fix v3
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * Update [vald sha: c3e5369387289ffa389c94edb3bc84cf10c454c7]
    
    * :green_heart: use checkout@v1
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * Update [vald sha: f1f1b5dd0af001d9ccb84e3c6827e61dc0b8cded]
    
    * Update [vald sha: 4eaba83814e242ed63280fac9b2743414374fc56]
    
    * Update [vald sha: c22fa3c3396f38796736f0ef4976c8a6881e404a]
    
    * Update [vald sha: 7d03cb0362f67668c2ab7048dd76933608c02f11]
    
    * Update [vald sha: fb784bdcdb93c82ae963b735e628d2f673bf311f]
    
    * :green_heart: use checkout@v3
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * Update [vald sha: fd6912fd8f71da96f88e4b859a44b3b87b037873]
    
    * Update [vald sha: c3fbac2d34abb439d63899635ce1ffb64463bb78]
    
    * Update [vald sha: c25a11184477a5f366cfb3209630f83813ca9559]
    
    * Update [vald sha: fad6a0adf21f67d2cf2c9fdb50749042388a2fdf]
    
    * :green_heart: update workflow
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * test
    
    * :green_heart: fix
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :green_heart: add new workflow
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :green_heart: fix
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * :green_heart: fix
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * Apply suggestions from code review
    
    * :green_heart: fix
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    * Update .github/workflows/update.yml

--- example-ts/example.ts
+++ example-ts/example.ts
@@ -11,6 +11,15 @@ const sclient = new v1_vald.search_grpc.SearchClient(
   "localhost:8081",
   grpc.credentials.createInsecure()
 );
+const rclient = new v1_vald.remove_grpc.RemoveClient(
+  "localhost:8081",
+  grpc.credentials.createInsecure()
+)
+
+const DIM = process.env.DIM || 4;
+const ID = "id_1"
+
+// sleep
 
 const sleep = async (second: number) => new Promise((resolve) => {
   console.log('Wait for ', second, 's');
@@ -18,11 +27,16 @@ const sleep = async (second: number) => new Promise((resolve) => {
 });
 
 const main = async () => {
+  const vec = [];
+  for (let i = 0; i < DIM; i++) {
+    vec.push(0.1 * (i + 1));
+  }
+
   // insert
 
   const ivec = new v1_payload.payload.Object.Vector();
-  ivec.setId("id_1");
-  ivec.setVectorList([0.1, 0.2, 0.3, 0.4]);
+  ivec.setId(ID);
+  ivec.setVectorList(vec);
 
   const icfg = new v1_payload.payload.Insert.Config();
   icfg.setSkipStrictExistCheck(false);
@@ -47,7 +61,7 @@ const main = async () => {
     console.log("resp: ", res);
   }).catch((e) => {
     console.log("err: ", e);
-    return
+    return -1
   });
 
   const second = 100
@@ -56,12 +70,13 @@ const main = async () => {
   // search
   const scfg = new v1_payload.payload.Search.Config();
   scfg.setNum(10);
+  scfg.setMinNum(1);
   scfg.setRadius(-1.0);
   scfg.setEpsilon(0.01);
   scfg.setTimeout(3000000000);
 
   const sreq = new v1_payload.payload.Search.Request();
-  sreq.setVectorList([0.1, 0.2, 0.3, 0.4]);
+  sreq.setVectorList(vec);
   sreq.setConfig(scfg);
 
   const searchFunc = (req: any) => {
@@ -79,8 +94,36 @@ const main = async () => {
     console.log('res: ', res, '\n');
   }).catch((e) => {
     console.log('err: ', e);
-    return
+    return -1
   });
-};
+
+  // remove
+  const rcfg = new v1_payload.payload.Remove.Config();
+  rcfg.setSkipStrictExistCheck(false)
+
+  const rreq = new v1_payload.payload.Remove.Request();
+  const robj = new v1_payload.payload.Object.ID();
+  robj.setId(ID);
+  rreq.setId(robj);
+  rreq.setConfig(rcfg);
+
+  const removeFunc = (req: any) => {
+    return new Promise((resolve, reject) => {
+      rclient.remove(req, (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(resp);
+        }
+      });
+    });
+  };
+  removeFunc(rreq).then((res: any) => {
+    console.log('res: ', res, '\n');
+  }).catch((e) => {
+    console.log('err: ', e);
+    return -1
+  });
+}
 
 main();
--- example/example.js
+++ example/example.js
@@ -1,26 +1,46 @@
-const grpc = require('@grpc/grpc-js');
-const vald = require('vald-client-node');
+const grpc = require("@grpc/grpc-js");
+const vald = require("vald-client-node");
 
 const insert = vald.v1_vald.insert_grpc;
 const search = vald.v1_vald.search_grpc;
+const remove = vald.v1_vald.remove_grpc;
 
 const payload = vald.v1_payload.payload;
 
 // create clients
-const iclient = new insert.InsertClient('localhost:8081', grpc.credentials.createInsecure());
-const sclient = new search.SearchClient('localhost:8081', grpc.credentials.createInsecure());
+const iclient = new insert.InsertClient(
+  "localhost:8081",
+  grpc.credentials.createInsecure()
+);
+const sclient = new search.SearchClient(
+  "localhost:8081",
+  grpc.credentials.createInsecure()
+);
+const rclient = new remove.RemoveClient(
+  "localhost:8081",
+  grpc.credentials.createInsecure()
+);
 
+const DIM = process.env.DIM || 4;
+const ID = "id_1"
 
-const sleep = (second) => new Promise((resolve) => {
-  console.log('Wait for ', second, 's');
-  setTimeout(resolve, second * 1000);
-});
+// sleep
+const sleep = (second) =>
+  new Promise((resolve) => {
+    console.log("Wait for ", second, "s");
+    setTimeout(resolve, second * 1000);
+  });
 
 const main = async () => {
+  const vec = [];
+  for (let i = 0; i < DIM; i++) {
+    vec.push(0.1 * (i + 1));
+  }
+
   // insert
   const ivec = new payload.Object.Vector();
-  ivec.setId("id_1");
-  ivec.setVectorList([0.1, 0.2, 0.3, 0.4]);
+  ivec.setId(ID);
+  ivec.setVectorList(vec);
 
   const icfg = new payload.Insert.Config();
   icfg.setSkipStrictExistCheck(false);
@@ -40,32 +60,36 @@ const main = async () => {
       });
     });
   };
-  insertFunc(ireq).then((res) => {
-    console.log('res: ', res);
-  }).catch((e) => {
-    console.log('err: ', e);
-    return
-  });
+  console.log("Insert start");
+  insertFunc(ireq)
+    .then((res) => {
+      console.log("res: ", res);
+    })
+    .catch((e) => {
+      console.log("err: ", e);
+      return -1;
+    });
 
   // Wait for createIndex completed
-  const second = 100;
+  const second = 120;
   await sleep(second);
 
   // search
   const scfg = new payload.Search.Config();
   scfg.setNum(10);
+  scfg.setMinNum(1);
   scfg.setRadius(-1.0);
   scfg.setEpsilon(0.01);
   scfg.setTimeout(3000000000);
 
   const sreq = new payload.Search.Request();
-  sreq.setVectorList([0.1, 0.2, 0.3, 0.4]);
+  sreq.setVectorList(vec);
   sreq.setConfig(scfg);
 
   const searchFunc = (req) => {
     return new Promise((resolve, reject) => {
       sclient.search(req, (err, resp) => {
-        if (err != null) {
+        if (err) {
           reject(err);
         } else {
           resolve(resp);
@@ -73,12 +97,44 @@ const main = async () => {
       });
     });
   };
-  searchFunc(sreq).then((res) => {
-    console.log('res: ', res, '\n');
-  }).catch((e) => {
-    console.log('err: ', e);
-    return
-  });
-}
+  console.log("Search start");
+  searchFunc(sreq)
+    .then((res) => {
+      console.log("res: ", res, "\n");
+    })
+    .catch((e) => {
+      console.log("err: ", e);
+      return -1;
+    });
 
-main()
+  const rcfg = new payload.Remove.Config();
+  rcfg.setSkipStrictExistCheck(false);
+  const robj = new payload.Object.ID();
+  robj.setId(ID)
+  const rreq = new payload.Remove.Request();
+  rreq.setId(robj.ID);
+  rreq.setConfig(rcfg);
+
+  const removeFunc = (req) => {
+    return new Promise((resolve, reject) => {
+      rclient.remove(req, (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(resp);
+        }
+      });
+    });
+  };
+  console.log("Remove start");
+  removeFunc(rreq)
+    .then((res) => {
+      console.log("res: ", res, "\n");
+    })
+    .catch((e) => {
+      console.log("err: ", e);
+      return -1;
+    });
+};
+
+main();
