0ec126d77a7f70e83e5fb8e658fe3db9dca08ba0
VDaaS org <ci@vdaas.org>
2023-03-29 08:52:18 +0000

    Update [vald sha: 26f01ea1a392262c26e64be72093e20fbd4d87f2]

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
+module.exports.v1_agent_core = require("./src/vald/v1/agent/core");
 module.exports.v1_vald = require("./src/vald/v1/vald");
 module.exports.v1_payload = require("./src/vald/v1/payload");
-module.exports.v1_agent_core = require("./src/vald/v1/agent/core");
