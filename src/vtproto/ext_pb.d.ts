02726f057be5a60b5435bcf2ccca2d56ae9839dd
vankichi <kyukawa315@gmail.com>
2023-12-21 01:01:17 +0900

    :wrench: apply buf
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- src/vtproto/ext_pb.d.ts
+++
@@ -1,8 +0,0 @@
-// package: vtproto
-// file: vtproto/ext.proto
-
-import * as jspb from "google-protobuf";
-import * as google_protobuf_descriptor_pb from "google-protobuf/google/protobuf/descriptor_pb";
-
-  export const mempool: jspb.ExtensionFieldInfo<boolean>;
-
