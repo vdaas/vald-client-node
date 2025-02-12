adc6ef7f547c9334d8306180473b097f6fc87234
Vdaas CI <vald@vdaas.org>
2025-02-12 09:16:33 +0000

    Update [vald sha: 085a838285c7dff4c5b298c6eb14373e20259afd]
    Signed-off-by: Vdaas CI <vald@vdaas.org>

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
-import v1_payload = require("./src/vald/v1/payload");
 import v1_agent_core = require("./src/vald/v1/agent/core");
 import v1_vald = require("./src/vald/v1/vald");
+import v1_payload = require("./src/vald/v1/payload");
 declare const _default: {
-    v1_payload: typeof v1_payload,
     v1_agent_core: typeof v1_agent_core,
     v1_vald: typeof v1_vald,
+    v1_payload: typeof v1_payload,
 };
 export = _default;
